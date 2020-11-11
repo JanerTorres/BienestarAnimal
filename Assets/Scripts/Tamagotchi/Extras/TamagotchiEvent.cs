e110a294719525bc0cffcbce4e0376648e82f830
janertorres <janer.torres@udea.edu.co>
2020-11-10 21:16:23 -0500

    nuevo repo

---
+++ Assets/Scripts/Tamagotchi/Extras/TamagotchiEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TamagotchiEvent : MonoBehaviour
+{
+    #region Singlenton
+
+    public static TamagotchiEvent instance;
+
+    private void Awake ()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public event Action <GameObject>OnCerdoSelec;
+
+    public event Action<string> OnCerdoCritico;
+
+
+    public void AsignarCerdo ( GameObject cerdoSel ) => OnCerdoSelec?.Invoke ( cerdoSel );
+
+    public void ChangeClip(string clipname) => OnCerdoCritico?.Invoke(clipname);
+
+}
---
+++ Assets/Scripts/Tamagotchi/SelectorCerdos.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorCerdos : MonoBehaviour
+{
+    public Vector3 posicionActual;
+    public Vector3 angulosActuales;
+    public MovimientoHexagonal movimientoHexagonal;
+    public GestorTamagotchi tama;
+    public bool enCentro;
+
+    void Start()
+    {
+        movimientoHexagonal = GetComponent<MovimientoHexagonal>();
+        tama = GetComponent<GestorTamagotchi>();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        if (MorionTools.MouseEnUI()) return;
+        if (!enCentro)
+        {
+            MoverAlCero();
+            GestionCerdoUI.singleton.Activar(tama, this);
+            tama.animCerdo.animator.SetFloat("velocidad", 0);
+        }
+    }
+
+    void MoverAlCero()
+    {
+        posicionActual = transform.position;
+        angulosActuales = transform.eulerAngles;
+        movimientoHexagonal.enabled = false;
+        transform.position = Vector3.zero;
+        transform.eulerAngles = Vector3.zero;
+        enCentro = true;
+    }
+
+    public void VolverAlPiso()
+    {
+        movimientoHexagonal.enabled = true;
+        transform.position = posicionActual;
+        transform.eulerAngles = angulosActuales;
+        enCentro = false;
+    }
+}
